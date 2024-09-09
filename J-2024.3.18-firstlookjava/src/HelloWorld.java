456a3c34bbccdd7e0fccfdda8439d56871eb3dd7
liangmou3434 <2685463636@qq.com>
2024-09-09 19:16:56 +0800

    第一次尝试提交代码到github

---
+++ J-2024.3.18-firstlookjava/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+    //输入main函数 直接输入main
+    public static void main(String[] args) {
+        //sout为打印函数
+        System.out.println("hello");
+        //输出10 先sout回车 再在括号输入10
+        System.out.println(10);
+        //输出10方法2 10.sout
+        System.out.println(10);
+        //写for循环 fori加回车
+        for (int i= 0; i <100; i++) {
+
+        }
+
+        }
+    }
+
