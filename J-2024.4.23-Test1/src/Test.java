510a4718b0bad4a950fc8a28454f3ece21404f56
liangmou3434 <2685463636@qq.com>
2024-09-10 19:06:51 +0800

    方法重载

---
+++ J-2024.4.23-Test1/src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+    //方法重载
+    //1 方法名称一致为Add 2 方法参数不一样(数据类型，个数，顺序)
+    public static int Add(int x,int y){//原方法
+
+    }
+    public static int Add(int x,int y,int z){//参数不同
+
+    }
+    public static int Add(int y,int x){//顺序不同
+
+    }
+    public static long Add(int x,int y){//返回值类型不同
+
+    }
+    public static double Add(double x,double y){//参数数据类型不同
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
