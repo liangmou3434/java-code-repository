5a76d78d05e3500e501cc03bba50b8ef9f30ed47
liangmou3434 <2685463636@qq.com>
2024-09-10 19:02:12 +0800

    if选择结构的写法

---
+++ J-2024.4.9-SchoolTest5/src/Test.java
@@ -0,0 +1,25 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: liang
+ * Date: 2024-04-09
+ * Time: 19:07
+ */
+import java.util.Scanner;
+public class Test {
+    //对于if选择结构的写法
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        if(number!=0){
+            System.out.println(number+"这个数字不是0");
+        }else{
+            System.out.println(number+"这个数字是0");
+        }
+
+    }
+    //注释代码和取消注释的快捷键 shift+ctrl+/ 或者ctrl+/
+
+}
+
+
