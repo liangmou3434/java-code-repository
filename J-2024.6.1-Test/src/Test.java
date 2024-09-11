263152219f1a3abb00bb90ac4ba2bb627862e9e4
liangmou3434 <2685463636@qq.com>
2024-09-11 22:38:29 +0800

    计算字符串长度

---
+++ J-2024.6.1-Test/src/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[] args) {
+        String s = "guangzhounanfangxueyuan";
+        int len = s.length();//计算字符串的长度的方法
+        System.out.println(len);
+    }
+}
